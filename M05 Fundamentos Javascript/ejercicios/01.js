0256eaf233e36407b43a94a20ea0ab11a40926f5
PaulV2G <paulfto2@outlook.com>
2024-01-23 09:16:01 -0700

    Hola_2
    Signed-off-by: PaulV2G <paulfto2@outlook.com>

--- M05 Fundamentos Javascript/ejercicios/01.js
+++ M05 Fundamentos Javascript/ejercicios/01.js
@@ -1,6 +1,6 @@
 // Crea una variable de tipo string.
 // Reemplaza el valor de null por el correspondiente.
 
-const nuevoString = "Paul Vicencio";
+const nuevoString = "Paul V";
 
 module.exports = nuevoString;
